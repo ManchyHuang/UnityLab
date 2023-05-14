8349cb499db034b849ee8f1a58d7340b02419719
ManchyHuang <hqq970598602@126.com>
2023-05-14 23:08:03 +0800

    DrawObjectPass

---
+++ BSRP/Assets/TInyRP/DrawObjectPass.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Manchy.Rendering.Tiny
+{
+    public class DrawObjectPass
+    {
+
+        public void Execute(ScriptableRenderContext context)
+        {
+
+        }
+    }
+}
